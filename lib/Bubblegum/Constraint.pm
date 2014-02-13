9334d3c5e87b78f99fe368e359b21a70e41cd7bd
Toby Inkster <mail@tobyinkster.co.uk>
2014-02-13 14:17:44 +0000

    Much faster (about 100 times faster) implementations of most of the functions in Bubblegum::Constraint.

--- lib/Bubblegum/Constraint.pm
+++ lib/Bubblegum/Constraint.pm
@@ -5,11 +5,24 @@ use utf8::all;
 use strict;
 use warnings;
 
-use Type::Params ();
-use Types::Standard ();
-
 use Scalar::Util 'blessed';
 use base 'Exporter::Tiny';
+
+# VERSION
+
+use Types::Standard (
+    assert_ArrayRef   => { -as => 'asa_arrayref' },
+    assert_CodeRef    => { -as => 'asa_coderef' },
+    assert_HashRef    => { -as => 'asa_hashref' },
+    assert_Ref        => { -as => 'asa_ref' },
+    assert_RegexpRef  => { -as => 'asa_regexp' },
+    assert_Str        => { -as => 'asa_string' },
+    is_ArrayRef       => { -as => 'isa_arrayref' },
+    is_Object         => { -as => 'isa_classref' },
+    is_CodeRef        => { -as => 'isa_coderef' },
+    is_HashRef        => { -as => 'isa_hashref' },
+    is_RegexpRef      => { -as => 'isa_regexp' },
+);
 
 our @EXPORT_OK  = qw(
     asa_arrayref
@@ -27,79 +40,8 @@ our @EXPORT_OK  = qw(
     isa_regexp
 );
 
-# VERSION
-
-sub asa_arrayref {
-    my $arrayref   = shift;
-    my $constraint = Types::Standard::ArrayRef;
-    Type::Params::compile($constraint)->($arrayref);
-    return $arrayref;
-}
-
-sub asa_coderef {
-    my $coderef    = shift;
-    my $constraint = Types::Standard::CodeRef;
-    Type::Params::compile($constraint)->($coderef);
-    return $coderef;
-}
-
-sub asa_hashref {
-    my $hashref    = shift;
-    my $constraint = Types::Standard::HashRef;
-    Type::Params::compile($constraint)->($hashref);
-    return $hashref;
-}
-
-sub asa_number {
-    my $number     = shift;
-    my $constraint = Types::Standard::Num;
-    Type::Params::compile($constraint)->($number);
-    return $number;
-}
-
-sub asa_ref {
-    my $reference  = shift;
-    my $constraint = Types::Standard::Ref;
-    Type::Params::compile($constraint)->($reference);
-    return $reference;
-}
-
-sub asa_regexp {
-    my $regexp     = shift;
-    my $constraint = Types::Standard::RegexpRef;
-    Type::Params::compile($constraint)->($regexp);
-    return $regexp;
-}
-
-sub asa_string {
-    my $string     = shift;
-    my $constraint = Types::Standard::Str;
-    Type::Params::compile($constraint)->($string);
-    return $string;
-}
-
-sub isa_arrayref {
-    return 'ARRAY' eq ref(shift) ? 1 : 0;
-}
-
-sub isa_classref {
-    return blessed(shift) ? 1 : 0;
-}
-
-sub isa_coderef {
-    return 'CODE' eq ref(shift) ? 1 : 0;
-}
-
 sub isa_evenlist {
     @_ % 2 ? 0 : 1
-}
-
-sub isa_hashref {
-    return 'HASH' eq ref(shift) ? 1 : 0;
-}
-
-sub isa_regexp {
-    return 'Regexp' eq ref(shift) ? 1 : 0;
 }
 
 1;
